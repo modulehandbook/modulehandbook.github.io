ad082719bd40d861a1cdad74635dc1499e9ab8e3
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-02-24 14:33:50 +0100

    ..

--- bin/gitversion.sh
+++ bin/gitversion.sh
@@ -1,5 +1,5 @@
 echo "commit: $(git rev-parse HEAD | cut -c 1-8)" > data/gitinfo.yml
-echo "tag:" $(git describe --tags) >> data/gitinfo.yml
+echo "tag:" $(git describe --tags) >> data/gitinfo.yml # "results in fatal: No names found, cannot describe anything."
 echo "ref:" $1 >> data/gitinfo.yml
 echo "sha:" $2 >> data/gitinfo.yml
 echo "workflow:" $3 >> data/gitinfo.yml
--- content/_index.md
+++ content/_index.md
@@ -6,4 +6,9 @@ weight: 1
 # geekdocHidden: false
 ---
 
-Welcome Site
+Module Handbook
+
+... Website coming soon.
+<p>
+<a href ="https://home.htw-berlin.de/~kleinen/software_projects/modulehandbook/">
+Link to Project Description on B. Kleinen's Page</a>
